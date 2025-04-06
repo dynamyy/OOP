df043c9ee0f4c272033b34036128720cfad5efe8
kevinkorrol <kevin.korrol@gmail.com>
2025-04-06 19:15:46 +0300

    Barbora praeguseks valmis

--- src/main/java/org/example/oop_projekt/OopProjektApplication.java
+++ src/main/java/org/example/oop_projekt/OopProjektApplication.java
@@ -58,7 +58,7 @@ public class OopProjektApplication {
         //selver.scrape();
 
         BarboraScraper barbora = new BarboraScraper(this.poodRepository);
-        barbora.kategooriaVahelehed();
+        barbora.scrape();
 
 
     }
