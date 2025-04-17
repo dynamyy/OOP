5aaf40d1a9deaaa56cab2610b8c60cfe988f85e4
tonislees <tonis.lees@gmail.com>
2025-04-18 00:21:15 +0300

    Tegin veebilehe jaoks põhja valmis, veel eriti midagi funktsionaalset ei lisanud.

---
+++ frontend/src/komponendid/Menuu.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Menuu() {
+    return (
+        <header className="menuu">
+            <nav>
+                <ul>
+                    <li><a href="">Kasutaja</a></li>
+                    <li><a href="">Ostukorvid</a></li>
+                    <li><a href="">Tooted</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Menuu;
---
+++ frontend/src/lehed/Ava.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Menuu from '../komponendid/Menuu'
+
+function Ava() {
+    return (
+        <>
+            <Menuu />
+            <div id='sisu' className='hele'>
+                <div className="tume2 umar-nurk" id='s-log'>
+                    <input type="text" className='hele tume-tekst' />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Ava;
