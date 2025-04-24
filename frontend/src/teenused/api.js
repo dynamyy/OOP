84bd78b6fef18c2e8fe890fa53d1a871d61ab60b
tonislees <tonis.lees@gmail.com>
2025-04-24 12:21:31 +0300

    teenused kaust frontendi

---
+++ frontend/src/teenused/api.js
@@ -0,0 +1,9 @@
+const BAAS_URL = "http://localhost:8080/api"
+
+export const postSisseLogimine = async (email, parool) => {
+    fetch(`${BAAS_URL}/sisse-logimine`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({email, parool})
+    })
+}
