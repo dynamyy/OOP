c7918fcae21bfb7705bd0934c32906bd299ee2f0
tonislees <tonis.lees@gmail.com>
2025-04-30 10:36:57 +0300

    Toodete järjekorra muutmine

--- backend/src/main/java/org/example/oop_projekt/andmepääsukiht/ToodeRepository.java
+++ backend/src/main/java/org/example/oop_projekt/andmepääsukiht/ToodeRepository.java
@@ -15,7 +15,8 @@ public interface ToodeRepository extends JpaRepository<Toode, Long> {
 
     @Query("SELECT new org.example.oop_projekt.DTO.ToodeDTO(t.nimetus, t.hindKliendi, t.hulgaHindKliendi, t.yhik, CASE WHEN t.hulgaHindKliendi < t.hindKliendi THEN 'true' ELSE 'false' END, t.pood.nimi) " +
             "FROM Toode t " +
-            "WHERE LOWER(t.nimetus) LIKE LOWER(:nimetus)")
+            "WHERE LOWER(t.nimetus) LIKE LOWER(:nimetus)" +
+            "ORDER BY 1")
     List<ToodeDTO> leiaToodeNimega(@Param("nimetus") String nimetus);
 
 
