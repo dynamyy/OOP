05010f227dd43573536f933decde84e07ab22e9a
tonislees <tonis.lees@gmail.com>
2025-05-14 01:45:50 +0300

    ostukorvi kuvamine

---
+++ frontend/src/komponendid/OstukorvPoodToodeKaart.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmarkCircle } from '@fortawesome/free-regular-svg-icons'
+
+
+function OstukorvPoodToodeKaart(props) {
+
+    return (
+        <div className='ostukorv-pood-toode-kaart'>
+            <div>
+                <div className="pilt-taust hele">
+                    <img src={props.pilt} alt="toote pilt" className='ostukorv-toode-pilt' />
+                </div>
+                <div>
+                    <span className="tekst-hele">- {props.kogus} {props.nimetus}</span>
+                </div>
+            </div>
+            <div>
+                <span>{props.hind} €/tk</span>
+                <span>{props.hind * props.kogus} €</span>
+            </div>
+        </div>
+    )
+}
+
+export default OstukorvPoodToodeKaart;
