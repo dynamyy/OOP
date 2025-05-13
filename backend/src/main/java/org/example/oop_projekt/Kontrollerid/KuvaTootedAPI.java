8bd900b6e0f4e91c717734c63b4b2fadf608dd5f
dynamyy <games.dynamy@gmail.com>
2025-05-14 01:25:50 +0300

    Toodete dünaamiline laadimine ostukorvide lisamise lehel

--- backend/src/main/java/org/example/oop_projekt/Kontrollerid/KuvaTootedAPI.java
+++ backend/src/main/java/org/example/oop_projekt/Kontrollerid/KuvaTootedAPI.java
@@ -1,9 +1,6 @@
 package org.example.oop_projekt.Kontrollerid;
 
-import org.example.oop_projekt.DTO.HinnaMuutusDTO;
-import org.example.oop_projekt.DTO.MarksonaDTO;
-import org.example.oop_projekt.DTO.TokenMarkSonaDTO;
-import org.example.oop_projekt.DTO.ToodeDTO;
+import org.example.oop_projekt.DTO.*;
 import org.example.oop_projekt.teenuskiht.ariloogika.ToodeTeenus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
